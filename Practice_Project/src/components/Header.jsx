71cd21861980512d65ad9cd2fe463c8d46f853e2
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-09-29 01:42:33 +0530

    header

--- Practice_Project/src/components/Header.jsx
+++ Practice_Project/src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-between'>
+        <div className='flex items-center justify-center gap-2'>
+            <img src="./assets/Logo.png" alt="logo" className='w-40 h-auto'/>
+            <button className='bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-1 py-1 rounded-2xl'> Practice  Project </button>
+        </div>
+        <div>
+            <FaBars />
+        </div>
+    </div>
   )
 }
 
