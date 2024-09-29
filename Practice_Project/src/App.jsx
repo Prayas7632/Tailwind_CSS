24e914f68636001e5869f6567bf5a5673129025a
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-09-29 11:42:33 +0530

    layout

--- Practice_Project/src/App.jsx
+++ Practice_Project/src/App.jsx
@@ -1,8 +1,16 @@
+import Body from "./components/Body"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
 function App() {
   
   return (
     <>
-      <div>Hello</div>
+      <div className="bg-amber-50 min-h-screen px-6 py-3 flex flex-col gap-16">
+        <Header/>
+        <Body/>
+        <Footer/>
+      </div>
     </>
   )
 }
