c4fcc0c1b5416c695289a15a3e28eefa0f89d5e6
Prayas Raj <97390918+Prayas7632@users.noreply.github.com>
2024-09-30 13:32:33 +0530

    footer

--- Practice_Project/src/components/Footer.jsx
+++ Practice_Project/src/components/Footer.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex flex-col gap-12'>
+        <ul className='flex gap-6 font-lato text-gray-400'>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Twitter</a></li>
+        </ul>
+        <div className='flex gap-6'>
+            <img src="./assets/help.png" alt="help" className='h-16'/>
+            <div>
+                <p className='font-playfair font-thin'>Practice with me</p>
+                <a href="#" className='font-lato font-medium'>Contact me</a>
+            </div>
+        </div>
+    </div>
   )
 }
 
